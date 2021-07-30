ea170d6faeb6ae3e4d74b1e4a366543e008ad382
om-mks <murayama@mukei-soft.co.jp>
2021-07-30 15:55:35 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+
+This is a repository of MUKEI-SOFT,LLC education system which can be started from junior high school.
+
+# link
+
+* [lesson](https://edu.mukei-soft.co.jp/)
+* [info](https://www.mukei-soft.co.jp/)
---
+++ js/kadai.js
@@ -0,0 +1,32 @@
+// 計算する。
+function keisan_suru() {
+    // TODO 3. 数値が入力されていなかったら、alertを表示して処理を終了する。
+    let seisu1 = parseInt(document.getElementById('seisu_1').value);
+    let seisu2 = parseInt(document.getElementById('seisu_2').value);
+    let enzanshi = document.getElementById('enzanshi').value;
+    var kotae;
+    if (enzanshi === '+') {
+        kotae = seisu1 + seisu2;
+    } // TODO 1. ここに他の計算プログラムを追加する。
+
+    // 答えを表示する。
+    document.getElementById('kotae').innerText = kotae;
+
+    // TODO 2. kotaeが３の倍数の場合、計算機を爆発させる。それ以外だったら計算機を普通に戻すためにリセットする。
+}
+
+// 計算機を爆発させる。
+function bakuhatsu() {
+    let keisanBox = document.getElementById('keisan-box');
+    keisanBox.setAttribute('class', 'keisan-box-bakuhatsu');
+    let kotaeLabel = document.getElementById('kotae');
+    kotaeLabel.setAttribute('class', 'kotae-bakuhatsu');
+}
+
+// 計算機をリセットする。
+function reset() {
+    let keisanBox = document.getElementById('keisan-box');
+    keisanBox.setAttribute('class', 'keisan-box-futuu');
+    let kotaeLabel = document.getElementById('kotae');    
+    kotaeLabel.setAttribute('class', 'kotae-futuu');
+}
